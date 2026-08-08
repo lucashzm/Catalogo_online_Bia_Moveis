12d2ad42f7bfe38042505d20f0a8844a71c87ebd
lucashzm <lukinhas99.bh@gmail.com>
2026-08-08 17:28:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const botoes = document.querySelectorAll(".filtro");
+const produtos = document.querySelectorAll(".produto");
 
+botoes.forEach(function(botao) {
+  botao.addEventListener("click", function() {
+
+    botoes.forEach(function(item) {
+      item.classList.remove("ativo");
+    });
+
+    botao.classList.add("ativo");
+
+    const categoria = botao.textContent.trim();
+
+    produtos.forEach(function(produto) {
+
+      const categoriaProduto = produto.dataset.categoria;
+
+      if (categoria === "Todos" || categoriaProduto === categoria) {
+        produto.style.display = "block";
+      } else {
+        produto.style.display = "none";
+      }
+
+    });
+
+  });
+});

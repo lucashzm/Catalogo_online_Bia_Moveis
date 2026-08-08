fee1c1e37bc4f95dd987433ffb0e317e76bcdb21
lucashzm <lukinhas99.bh@gmail.com>
2026-08-08 18:03:25 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,9 @@
+const produtos = [
+  {
+    nome: "Sofá 3 lugares",
+    categoria: "Sofás",
+    imagem: "Estofado Living Felicia 180 3 Lgs Linho Inox Viero.jpg",
+    descricao: "Confortável e moderno",
+    preco: "R$ 1.199,00"
+  }
+];

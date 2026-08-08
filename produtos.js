231bc2b396d578cd8943d59addbd8e5d19ecaf23
lucashzm <lukinhas99.bh@gmail.com>
2026-08-08 20:51:16 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,6 +1,6 @@
 const produtos = [
   {
-    nome: "Sofá Living Felicia 180 3 Lgs Linhão Nanquim",
+    nome: "Sofá Living Felicia 180 3Lugares Linhão Nanquim",
     categoria: "Sofás",
     imagem: "imagens/sofas/Estofado Living Felicia 180 3 Lgs Linho Inox Viero.jpg",
     descricao: "Estofado Living Felicia 180 3 Lugares, Tecido Linhão, Cor Nanquim",
@@ -8,18 +8,18 @@ const produtos = [
   },
 
   {
-    nome: "Roupeiro Star 2P 6G Cinamomo/Off White",
+    nome: "Roupeiro Star 2Portas 6Gavetas Cinamomo/Off White",
     categoria: "Roupeiros",
     imagem: "imagens/roupeiros/Roupeiro Star 2P6G Cinamomo Off White.jpg",
-    descricao: "Roupeiro Star, Cor Cinamomo/Off White",
+    descricao: "Roupeiro Star, 2 Portas, 6 Gavetas, Cor Cinamomo/Off White",
     preco: "R$ 953,00"
   },
   
   {
-    nome: "Comoda Perola Fluence Plus 8G Freijo",
+    nome: "Comoda Perola Fluence Plus 8Gavetas Freijo",
     categoria: "Cômodas",
     imagem: "imagens/comodas/Comoda Perola Fluence Plus 8G Freijo.jpg",
-    descricao: "Cômoda Pérola Fluence Plus, Cor Freijó",
+    descricao: "Cômoda Pérola Fluence Plus 8 Gavetas, Cor Freijó",
     preco: "R$ 480,00"
   }
     

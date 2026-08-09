22fce3a7beb078debd5dbf3f0d392698c98caa4a
lucashzm <lukinhas99.bh@gmail.com>
2026-08-09 14:19:32 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -27,7 +27,7 @@ const produtos = [
     nome: "Roupeiro Vivacce 4Portas4Gavetas Cinamomo Off White",
     categoria: "Roupeiros",
     imagem: "imagens/roupeiros/Roupeiro Vivacce 4P4G Cinamomo Off White 4V Albatroz.jpg",
-    descricao: "Roupeiro Vivvace, 4 Portas, 4 Gavetas, Cor Cinamomo Off White",
+    descricao: "Roupeiro Vivvace 4 Portas, 4 Gavetas, Cor Cinamomo Off White",
     preco: "R$ 1.399,99"
   },
 
@@ -37,6 +37,14 @@ const produtos = [
     imagem: "imagens/roupeiros/Roupeiro Madri 6P Vidro Reflecta Peroba 10V.jpg",
     descricao: "Roupeiro Madri 6 Portas de vidro Reflecta Peroba",
     preco: "R$ 3.700,00"
+  },
+
+  {
+    nome: "Roupeiro Camarim Olimpia 4Portas 2Gavetas Cinamomo/Salvia",
+    categoria: "Roupeiros",
+    imagem: "imagens/roupeiros/Roupeiro Camarim Olimpia 4P2G Cinamomo Salvia.jpg",
+    descricao: "Roupeiro Camarim Olimpia 4 Portas 2 Gavetas, Cor Cinamomo/Salvia",
+    preco: "R$ 983,00"
   }
       
       
